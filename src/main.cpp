99d244fd91b3e2cd676a424516dd446d4f095974
ajh123 <ajh123@users.noreply.github.com>
2023-09-02 19:16:52 +0100

    again

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 {
     if (argc != 2) {
         std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
-        std::cerr << "ezscript <input.ezscript>" << std::endl;
+        std::cerr << "ezscript <input.ez>" << std::endl;
         return EXIT_FAILURE;
     }
 
